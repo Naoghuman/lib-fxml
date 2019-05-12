c51211439332aca8566bce3a1589f73524dfcf41
Naoghuman <peter.rogge@yahoo.de>
2019-05-12 20:50:42 +0200

    #13 [test] Start with unittests in FXMLActionTest.

---
+++ src/test/java/com/github/naoghuman/lib/fxml/core/FXMLActionTest.java
@@ -0,0 +1,163 @@
+/*
+ * Copyright (C) 2019 Naoghuman's dream
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package com.github.naoghuman.lib.fxml.core;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ *
+ * @author PRo
+ */
+public class FXMLActionTest {
+    
+    public FXMLActionTest() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    @Test
+    public void testGetDefault() {
+//        System.out.println("getDefault");
+//        FXMLAction expResult = null;
+//        FXMLAction result = FXMLAction.getDefault();
+//        assertEquals(expResult, result);
+//        fail("The test case is a prototype.");
+    }
+
+    @Test
+    public void testHandleAction_String() {
+//        System.out.println("handleAction");
+//        String actionId = "";
+//        FXMLAction instance = null;
+//        instance.handleAction(actionId);
+//        fail("The test case is a prototype.");
+    }
+
+    @Test
+    public void testHandleAction_String_Object() {
+//        System.out.println("handleAction");
+//        String actionId = "";
+//        Object source = null;
+//        FXMLAction instance = null;
+//        instance.handleAction(actionId, source);
+//        fail("The test case is a prototype.");
+    }
+
+    @Test
+    public void testHandleConsumer() {
+//        System.out.println("handleConsumer");
+//        String actionId = "";
+//        FXMLModel model = null;
+//        FXMLAction instance = null;
+//        instance.handleConsumer(actionId, model);
+//        fail("The test case is a prototype.");
+    }
+
+    @Test
+    public void testHandleFunction() {
+//        System.out.println("handleFunction");
+//        String actionId = "";
+//        Long entityId = null;
+//        FXMLAction instance = null;
+//        Optional<FXMLModel> expResult = null;
+//        Optional<FXMLModel> result = instance.handleFunction(actionId, entityId);
+//        assertEquals(expResult, result);
+//        fail("The test case is a prototype.");
+    }
+
+    @Test
+    public void testHandleSupplier() {
+//        System.out.println("handleSupplier");
+//        String actionId = "";
+//        FXMLAction instance = null;
+//        Optional<List<FXMLModel>> expResult = null;
+//        Optional<List<FXMLModel>> result = instance.handleSupplier(actionId);
+//        assertEquals(expResult, result);
+//        fail("The test case is a prototype.");
+    }
+
+    @Test
+    public void testIsRegistered() {
+//        System.out.println("isRegistered");
+//        String actionId = "";
+//        FXMLAction.Type type = null;
+//        FXMLAction instance = null;
+//        boolean expResult = false;
+//        boolean result = instance.isRegistered(actionId, type);
+//        assertEquals(expResult, result);
+//        fail("The test case is a prototype.");
+    }
+
+    @Test
+    public void testRegister_String_Consumer() {
+//        System.out.println("register");
+//        String actionId = "";
+//        Consumer<FXMLModel> consumer = null;
+//        FXMLAction instance = null;
+//        instance.register(actionId, consumer);
+//        fail("The test case is a prototype.");
+    }
+
+    @Test
+    public void testRegister_String_EventHandler() {
+//        System.out.println("register");
+//        String actionId = "";
+//        EventHandler<ActionEvent> eventHandler = null;
+//        FXMLAction instance = null;
+//        instance.register(actionId, eventHandler);
+//        fail("The test case is a prototype.");
+    }
+
+    @Test
+    public void testRegister_String_Function() {
+//        System.out.println("register");
+//        String actionId = "";
+//        Function<Long, FXMLModel> function = null;
+//        FXMLAction instance = null;
+//        instance.register(actionId, function);
+//        fail("The test case is a prototype.");
+    }
+
+    @Test
+    public void testRegister_String_Supplier() {
+//        System.out.println("register");
+//        String actionId = "";
+//        Supplier<List<FXMLModel>> supplier = null;
+//        FXMLAction instance = null;
+//        instance.register(actionId, supplier);
+//        fail("The test case is a prototype.");
+    }
+
+    @Test
+    public void testRemove() {
+//        System.out.println("remove");
+//        String actionId = "";
+//        FXMLAction.Type type = null;
+//        FXMLAction instance = null;
+//        instance.remove(actionId, type);
+//        fail("The test case is a prototype.");
+    }
+    
+}
